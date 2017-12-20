d49834667b98d0157d89c3accce5400934d4630a
Haocen <Haocen@users.noreply.github.com>
2017-12-20 19:36:58 +0000

    Create chinese_fonts_input.sh

---
+++ src/ubuntu/install/chinese_fonts_input.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+echo "Install Chinese fonts and input"
+RUN apt-get update
+RUN apt-get install -y \
+  fontconfig \
+  ttf-wqy-zenhei \
+  ttf-wqy-microhei \
+  fonts-horai-umefont \
+  ibus-pinyin
